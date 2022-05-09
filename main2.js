d2d973927772d2a641daf5714b633f6cc875119d
RedRoses24 <87375785+RedRoses24@users.noreply.github.com>
2022-05-09 17:53:33 +0530

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,30 @@
+function preload(){
+}
+function setup(){
+canvas=createCanvas(300, 300);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+}
+function modelLoaded(){
+   console.log("PoseNet is initialized");
+}
+function gotPoses(results){
+   if(results.length>0){
+       console.log(results);
+       console.log("Right eye x= "+results[0].pose.rightEye.x);
+       console.log("Right eye y= "+results[0].pose.rightEye.y);
+   }
+}
+function take_snapshot(){
+    save('Sunglasses_Me_Img.png');
+}
+function back(){
+    window.location="index.html";
+}
